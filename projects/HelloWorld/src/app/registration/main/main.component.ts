c1aaf7ea3bbae2e3625484e67eb98522331e7eab
gmuvvala <gopikrishna.crl@gmail.com>
2018-12-29 00:03:11 -0600

    added templated based form  in registration module

--- projects/HelloWorld/src/app/registration/main/main.component.ts
+++ projects/HelloWorld/src/app/registration/main/main.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RegModel } from './regModel.model';
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'registration-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  model = new RegModel("first name","last name","111-111-1111");
+  submitted = false;
+  constructor() {
+    }
 
   ngOnInit() {
+    }
+  onSubmit(){
+    this.submitted = true;
   }
-
 }
